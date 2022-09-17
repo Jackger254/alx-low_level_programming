272b327af384e2ecb5ddb0e888479b6618810644
Jackton Onyango <jacktonger@gmail.com>
2022-09-17 07:03:49 -0700

    fixed minor errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * main - prints the size f various types
+ * main - prints the size of various types
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void) 
