cad4bc834c9190b9039d18d55b0bf3c69172f220
Jackton Onyango <jacktonger@gmail.com>
2022-09-23 05:42:09 -0700

    rechecked betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
- * return: 0 if exited properly , non-zero otherwise 
+ * return:0 if exited properly,non-zero otherwise
  */
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-\n
+	puts("\"Programming is like building a multilingual puzzle"\n);
+	return (0);
 }
