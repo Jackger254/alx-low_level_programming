430318452dfc6296385831b250f41e17209b2438
Jackger254 <jacktonger15@gmail.com>
2023-07-15 05:04:53 -0700

    first printf program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art,
+ * using the function printf
+ *
+ * return: 0 if executed successfully, non-zero otherwise
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
