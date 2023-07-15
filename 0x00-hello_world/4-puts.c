eb1b6598085353da8c2d0ae8440a2615e1afdf5a
Jackger254 <jacktonger15@gmail.com>
2023-07-15 04:27:37 -0700

    fixed betty error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main-prints "Programming is like building a multilingual puzzle
- *using the function puts
- * retun - 0 if exited properly,non zero otherwise
+ * main - prints Programming is like building a multilingual puzzle
+ * return:0 if successful
  */
 int main(void)
 {
